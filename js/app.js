1f80ed66047a00d86fc13273d8305033433bed16
Александр Анисимов <sanisimov@192.168.1.115>
2022-07-15 17:11:15 +0100

    added cv download

--- js/app.js
+++ js/app.js
@@ -1,19 +1,16 @@
 // select all slills
 const skills = document.querySelectorAll('.skills-cards')
-// listen for scroll event and call the checkBoxes function every time the user scrolls 
+// listen for scroll event and call the checkSkills function every time the user scrolls 
 window.addEventListener('scroll', checkSkills)
 // call function on pageload so the first boxes are displayed
+// variable stores the point at which you want the element to move into the page. In this example at 80% from the top.
+const triggerBottom = window.innerHeight / 5 * 4
 checkSkills()
 
-
 function checkSkills() {
-       // variable stores the point at which you want the element to move into the page. In this example at 80% from the top.
- const triggerBottom = window.innerHeight / 5 * 4
-
     skills.forEach(skill => {
       // check the location of the box on the page
         const boxTop = skill.getBoundingClientRect().top
-
         // show the box when its further down the page then the 80% from the top
         if(boxTop < triggerBottom) {
             skill.classList.add('show-skills')
@@ -31,13 +28,9 @@ window.addEventListener('scroll', checkPortfolio)
 checkPortfolio()
 
 function checkPortfolio() {
-     // variable stores the point at which you want the element to move into the page. In this example at 80% from the top.
- const triggerBottom = window.innerHeight / 5 * 4
-
  portfolio.forEach(port => {
       // check the location of the box on the page
         const boxTop = port.getBoundingClientRect().top
-
         // show the box when its further down the page then the 80% from the top
         if(boxTop < triggerBottom) {
             port.classList.add('port')
@@ -47,26 +40,17 @@ function checkPortfolio() {
     })
 }
 
-
-
 // select all boxes
 const boxes = document.querySelectorAll('.timeline')
 // listen for scroll event and call the checkBoxes function every time the user scrolls 
 window.addEventListener('scroll', checkBoxes)
 // call function on pageload so the first boxes are displayed
-
-
-
 checkBoxes()
 
 function checkBoxes() {
-     // variable stores the point at which you want the element to move into the page. In this example at 80% from the top.
- const triggerBottom = window.innerHeight / 5 * 4
-
     boxes.forEach(box => {
       // check the location of the box on the page
         const boxTop = box.getBoundingClientRect().top
-
         // show the box when its further down the page then the 80% from the top
         if(boxTop < triggerBottom) {
             box.classList.add('show')
